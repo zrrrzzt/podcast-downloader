71051b8f6694ba951d99b5b7fd71f9177e89255e
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-21 19:42:03 +0100

    Initial commit

---
+++ test/testLevelDB.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var assert = require('assert');
+var db = require('../lib/leveldb');
+
+describe('dbPut', function() {
+
+  it('it requires an options object', function(done) {
+
+    var options = false;
+
+    db.put(options, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required input: options/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+
+  it('it requires options.key to exist', function(done) {
+
+    var options = {
+      key: false
+    };
+
+    db.put(options, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required input: options.key/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+
+  it('it requires options.value to exist', function(done) {
+
+    var options = {
+      key: true,
+      value:false
+    };
+
+    db.put(options, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required input: options.value/.test(err)) {
+            return true;
+          }
+        },
+        'Unexpected error'
+      );
+      done();
+    });
+  });
+});
